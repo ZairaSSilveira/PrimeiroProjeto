cf8d04032b86aab7cfb5007ce12a34128db1a6a4
Zaira <133662712+ZairaSSilveira@users.noreply.github.com>
2024-04-17 16:31:22 -0300

    oi

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int idade = 68;
+             Console.WriteLine("A minha idade é: " + idade);*/
+            /*
+
+                        int numero1 = 3;
+                        int numero2 = 4;
+
+                        int soma = numero1 + numero2;
+                        int subtracao = numero1 - numero2;
+                        int multiplicacao = numero1 * numero2;
+                        int divisao = numero1 / numero2;
+
+                        Console.WriteLine(soma);
+                        Console.WriteLine(subtracao);
+                        Console.WriteLine(multiplicacao);
+                        Console.WriteLine(divisao);*/
+            /*
+                        double salario = 1500.86;
+                        Console.WriteLine(salario);*/
+
+
+            /* int numero1 = 35;
+             int numero2 = 8;
+
+             double multiplicacao = numero1 * numero2;
+
+             Console.WriteLine(multiplicacao);*/
+            /*
+                        int numero1 = 27;
+                        int numero2 = 16;
+
+                        int multiplicacao = numero1 * numero2;
+
+                        Console.WriteLine(multiplicacao);*/
+
+            /*int numero1 = 1;
+            int numero2 = 2;
+
+            int soma = numero1 + numero2;
+
+            Console.WriteLine(soma);
+*//*
+            int numero1 = 300;
+            int numero2 = 20;
+
+            int divisao = numero1 / numero2;
+
+            Console.WriteLine(divisao);*/
+
+
+            /*   double instalaçao = 300.00;
+               double limpeza = 150.00;
+               double manutencao = 499.99;
+               double desconto = 50.00;
+
+               double resultado = instalaçao + limpeza + manutencao - desconto;
+
+               Console.WriteLine(resultado);
+
+
+   */
+
+           /*
+            Console.Write("Digite o primeiro número inteiro: ");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo número inteiro: ");
+
+            int numero2 = int.Parse(Console.ReadLine());
+            int soma = numero1 + numero2;
+
+            Console.WriteLine("A soma dos números é: " + soma);
+*/
+
+
+
+
+
+
+
+        }
+    }
+}
+
